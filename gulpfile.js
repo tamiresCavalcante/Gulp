328decb87b4ca1707872a08ed0f45dea596ae9db
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2021-06-10 13:50:07 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+function compilaSass(){
+    return gulp
+    .src('css/scss/*.scss')
+    .pipe(sass())
+    pipe(gulp.dest('css/'))
+}
+
+gulp.task('sass', compilaSass);
