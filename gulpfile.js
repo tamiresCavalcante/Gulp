a575e5d682155b0e5bd2bb4cf0d0afd8216239a9
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2021-06-11 18:27:04 -0300

    Second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 function compilaSass(){
     return gulp
     .src('css/scss/*.scss')
-    .pipe(sass())
-    pipe(gulp.dest('css/'))
+    .pipe(sass({output: 'compressed'}))
+    .pipe(autoprefixer({
+        browser: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('css/'))
 }
 
 gulp.task('sass', compilaSass);
+
+function browser(){
+    browserSync.init({
+        server: {
+            baseDir:"./"
+        }
+    })
+}
+
+function watch() {
+    gulp.watch('css/scss/*.scss', compilaSass)
+}
+
+gulp.task('default', watch);
+
