88c8cd17873bc2942e4122ec85ca3142cd454865
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2021-06-12 14:51:42 -0300

    Gulp Concat

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
-
+const concat = require('gulp-concat');
 
 //Função para compilar o SASS e adicionar os prefixos
 function compilaSass(){
@@ -23,6 +23,16 @@ function compilaSass(){
 //Tarefa de gulp para função de SASS
 gulp.task('sass', compilaSass);
 
+//Função para juntar op JS
+function gulpJS(){
+    return gulp
+    .src('js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js/'))
+}
+
+gulp.task('mainjs', gulpJS);
+
 // Função para iniciar o browser
 function browser(){
     browserSync.init({
@@ -38,6 +48,7 @@ gulp.task('browser-sync', browser);
 // Função de watch do gulp
 function watch() {
     gulp.watch('css/scss/*.scss', compilaSass)
+    gulp.watch(['js/*.js', '!js/main.js'], gulpJS)
     gulp.watch(['*.html', '*.php']).on('change', browserSync.reload);
 }
 
@@ -45,5 +56,5 @@ function watch() {
 gulp.task('watch', watch);
 
 //Tarefa padrão do gulp, que inicia o watch e o browser-sync
-gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs'));
 
