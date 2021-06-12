125e1fd1ff6bc8fb13f613e90465a49a2dc41ea4
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2021-06-12 14:08:33 -0300

    Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,49 @@
+//Adiciona os modulso instalados
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+
+//Função para compilar o SASS e adicionar os prefixos
 function compilaSass(){
     return gulp
     .src('css/scss/*.scss')
-    .pipe(sass({output: 'compressed'}))
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
     .pipe(autoprefixer({
         browser: ['last 2 versions'],
         cascade: false
     }))
     .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream())
 }
 
+//Tarefa de gulp para função de SASS
 gulp.task('sass', compilaSass);
 
+// Função para iniciar o browser
 function browser(){
     browserSync.init({
         server: {
             baseDir:"./"
         }
-    })
+    });
 }
 
+//Tarefa para iniciar o browser-sync
+gulp.task('browser-sync', browser);
+
+// Função de watch do gulp
 function watch() {
     gulp.watch('css/scss/*.scss', compilaSass)
+    gulp.watch(['*.html', '*.php']).on('change', browserSync.reload);
 }
 
-gulp.task('default', watch);
+//Inicia a tarefa de watch
+gulp.task('watch', watch);
+
+//Tarefa padrão do gulp, que inicia o watch e o browser-sync
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
 
